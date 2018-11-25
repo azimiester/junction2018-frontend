57ffae783f09bdadc1b67a0a6188dfe65acdd4b7
Azeem Akhter <azeem.akhter@futurice.com>
2018-11-25 02:14:51 +0200

    add stuff

--- src/App.js
+++ src/App.js
@@ -10,18 +10,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lat: undefined,
-      lng: undefined,
-      markers: []
+      center: null,
+      markers: [],
+      polyCords: null
     };
-  }
-  async requestApi(lat, lng) {
-    const getdata = await axios.get(
-      `http://gravity02-dev.azurewebsites.net/api/events?lat=${lat}&lon=${lng}&range=${2000}`
-    );
-    this.setState({
-      markers: getdata.data
-    });
   }
   render() {
     return (
@@ -29,16 +21,32 @@ class App extends Component {
         <p>Hello</p>
         <Autocomplete
           style={{ width: "90%" }}
-          onPlaceSelected={place => {
-            console.log(place);
+          onPlaceSelected={async place => {
+            const { lat, lng } = place.geometry.location;
+            this.setState({ center: { lat: lat(), lng: lng() } });
+            const apiData = await axios.get(
+              `http://10.100.31.58:8888/grid_id?lat=${lat()}&long=${lng()}`
+            );
+            const polyCords = apiData.data.poly_cords;
+            const RafayApiData = await axios.get(
+              `http://gravity02-dev.azurewebsites.net/api/events?lat=${lat()}&lon=${lng()}&range=${1000}`
+            );
+            if (polyCords) {
+              this.setState({
+                polyCords: polyCords.map(p => ({ lat: p[0], lng: p[1] })),
+                markers: RafayApiData.data
+              });
+            }
           }}
           types={["(regions)"]}
           componentRestrictions={{ country: "fi" }}
         />
 
         <Map
+          center={this.state.center}
           markers={this.state.markers}
-          onMapClick={this.requestApi.bind(this)}
+          polyCords={this.state.polyCords}
+          onMapClick={() => {}}
         />
       </div>
     );
