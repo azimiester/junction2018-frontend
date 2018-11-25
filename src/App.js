a681f09f7c931e804e543aa86b0f4e60fb97adf5
Abdullah Danyal Saeed <abdullah.saeed@isolta.com>
2018-11-25 02:48:15 +0200

    Add styles

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import axios from "axios";
 import Map from "./Map";
 import { GoogleApiWrapper } from "google-maps-react";
 import Autocomplete from "react-google-autocomplete";
-
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import TagsInput from 'react-tagsinput'
+import 'react-tagsinput/react-tagsinput.css' // If using WebPack and style-loader.
 import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lat: undefined,
-      lng: undefined,
-      markers: []
+    state = {
+        startDate: new Date(),
+        lat: undefined,
+        lng: undefined,
+        markers: [],
+        tags: []
     };
-  }
+
   async requestApi(lat, lng) {
     const getdata = await axios.get(
       `http://gravity02-dev.azurewebsites.net/api/events?lat=${lat}&lon=${lng}&range=${2000}`
@@ -23,23 +26,32 @@ class App extends Component {
       markers: getdata.data
     });
   }
+
+  onChange = date => this.setState({ startDate: date });
+
+    handleTagChange = (tags) => {
+        this.setState({tags})
+    };
+
   render() {
     return (
-      <div>
-        <p>Hello</p>
-        <Autocomplete
-          style={{ width: "90%" }}
-          onPlaceSelected={place => {
-            console.log(place);
-          }}
-          types={["(regions)"]}
-          componentRestrictions={{ country: "fi" }}
-        />
-
-        <Map
-          markers={this.state.markers}
-          onMapClick={this.requestApi.bind(this)}
-        />
+      <div className="main-app-container">
+        <h3>Telia Challenge</h3>
+          <div className="input-container">
+              <Autocomplete
+                  style={{width: '40%'}}
+                  onPlaceSelected={(place) => {
+                      console.log(place);
+                  }}
+                  types={['(regions)']}
+                  componentRestrictions={{country: "ru"}}
+              />
+              <DatePicker
+                  selected={this.state.startDate}
+                  onChange={this.onChange}
+              />
+              <TagsInput value={this.state.tags} onChange={this.handleTagChange} />
+          </div>
       </div>
     );
   }
